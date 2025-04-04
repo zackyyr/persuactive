80afcd6ac7f20b95881b69e1043aafca33ba3800
Zacky Raechan <muhamadraechanulwanzacky@Jake.local>
2025-04-05 03:41:44 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+    const navToggle = document.querySelector('.hamburger-menu button');
+    const navLinks = document.querySelector('.nav-link');
+
+    function openNavbar() {
+        navLinks.classList.toggle('active');
+    }
+
+    // Close menu when clicking outside
+    document.addEventListener('click', function(event) {
+        const isClickInsideNav = navLinks.contains(event.target);
+        const isClickOnToggle = navToggle.contains(event.target);
+
+        if (!isClickInsideNav && !isClickOnToggle) {
+            navLinks.classList.remove('active');
+        }
+    });
+
+    // Optional: auto close nav on link click (mobile)
+    document.querySelectorAll('.nav-link a').forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+        });
+    });
+
+
+    window.addEventListener('scroll', () => {
+        const header = document.querySelector('.header');
+        header.classList.toggle('scrolled', window.scrollY > 10);
+    });
